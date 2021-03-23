24fed39f057fdb3f8e48f0e000a3cf3d93db83f8
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-23 14:12:00 +0200

    Add files via upload

---
+++ Lab3.3C.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include "Vector3D.h"
+
+int main()
+{
+    cout << "Number of elements of class Vector3D : " << Object::Count() << endl;
+
+    Vector3D A;
+    cout << "Number of elements of class Vector3D : " << Object::Count() << endl;
+
+    Vector3D B;
+    cout << "Number of elements of class Vector3D : " << Object::Count() << endl;
+
+    Vector3D C;
+    cout << "Number of elements of class Vector3D : " << Object::Count() << endl;
+
+    cout << "\nVector A" << endl;
+    cin >> A;
+    cout << A;
+
+    cout << "\nVector B" << endl;
+    cin >> B;
+    cout << B;
+
+    cout << "\nVector C" << endl;
+    cin >> C;
+    cout << C;
+
+    cout << "\nA + B = " << endl;
+    cout << A + B << endl;
+
+    cout << "\nB + C = " << endl;
+    cout << B + C << endl;
+
+    cout << "\nA + C = " << endl;
+    cout << A + C << endl;
+
+    cout << "\nA * B = " << A * B << endl;
+
+    cout << "A++" << endl; cout << A++ << endl;
+    cout << "A--" << endl; cout << A-- << endl;
+    cout << "++A" << endl; cout << ++A << endl;
+    cout << "--A" << endl; cout << --A << endl;
+
+    cout << "Size of class is equal to " << sizeof(Vector3D);
+
+    return 0;
+}
---
+++ UnitTest3.3C.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab3.3C/Lab3.3C/Vector3D.h"
+#include "../Lab3.3C/Lab3.3C/Vector3D.cpp"
+#include "../Lab3.3C/Lab3.3C/Object.h"
+#include "../Lab3.3C/Lab3.3C/Object.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest33C
+{
+	TEST_CLASS(UnitTest33C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Vector3D A(1, 2, 3), B(4, 5, 6);
+			//A * B	
+			Assert::AreEqual(A * B, 32.);
+		}
+	};
+}
